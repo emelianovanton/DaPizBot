e4907c10834b40ff839fa1c0213dc37f8f56d028
aemelyanov-ms <100220985+aemelyanov-ms@users.noreply.github.com>
2022-09-30 14:49:14 +0300

    add random sleep before answering

--- bot.py
+++ bot.py
@@ -2,6 +2,9 @@ import telebot
 import re
 import os
 
+from random import randint
+from time import sleep
+
 TOKEN = os.environ["TOKEN"]
 
 bot = telebot.TeleBot(TOKEN)  # YOUR BOT TOKEN SHOULD BE HERE
@@ -10,6 +13,7 @@ bot = telebot.TeleBot(TOKEN)  # YOUR BOT TOKEN SHOULD BE HERE
 @bot.message_handler(content_types=['text'])
 def da_pizda_replies(message):
     text_message = re.sub(r'[^А-Яа-я]', '', message.text)
+    sleep(randint(1, 5))
     if str.lower(text_message) == 'да':
         bot.reply_to(message, "Пизда!")
     elif str.lower(text_message) == 'пизда':
